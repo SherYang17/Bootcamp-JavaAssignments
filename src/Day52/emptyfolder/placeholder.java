f55f6df4f272ae2fe3bc92e4e462e6262bb73b72
sheryang17 <yangs15000@gmail.com>
2023-05-16 12:26:35 -0400

    uploading assignments for day 52

---
+++ src/Day52/emptyfolder/placeholder.java
@@ -0,0 +1,7 @@
+package Day52.emptyfolder;
+
+public class placeholder {
+    public static void main(String[] args) {
+        System.out.println("Just a placeholder for package structure");
+    }
+}
